a90fed20a0d2872783f82e7c7c1f6da102eb3b1c
LuzianHahn <luzian@hahn-coburg.de>
2023-09-29 14:03:20 +0200

    fix: mv installer to $HOME
    also apply unified indention formatting

--- .local/installer/dotfile_installer.sh
+++ .local/installer/dotfile_installer.sh
@@ -1,16 +1,19 @@
-git clone --bare https://github.com/LuzianHahn/dotfiles.git $HOME/.cfg
-function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-}
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
+(
+  cd $HOME
+  git clone --bare https://github.com/LuzianHahn/dotfiles.git $HOME/.cfg
+  function config {
+    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+  }
+  mkdir -p .config-backup
+  config checkout
+  if [ $? = 0 ]; then
+    echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-	echo "Backup is located at \"$PWD/.config-backup\""
-fi;
-config checkout
-config config --local status.showUntrackedFiles no
-( cd $HOME && config submodule update --init --recursive)
+    echo "Backup is located at \"$PWD/.config-backup\""
+    config checkout
+  fi;
+  config config --local status.showUntrackedFiles no
+  config submodule update --init --recursive
+)
